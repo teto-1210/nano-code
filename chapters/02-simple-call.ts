76bd0c82425126386567bf639195cc28a647e5c3
teto-1210 <wnnyiwaki.academic@gmai.com>
2026-05-05 11:56:16 +0000

    complete chapter2

---
+++ chapters/02-simple-call.ts
@@ -0,0 +1,30 @@
+
+// interface ChatCompletionMessage {
+//     role: "system" | "user" | "assistant";
+//     content: string;
+// }
+
+async function callOpenAI() {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', 
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+            },
+            body: JSON.stringify({
+                model: "gpt-5-mini",
+                messages: [
+                    { role: "user", content: "TypeScriptについて簡潔に説明してください." },
+                ]
+            })
+        }
+    );
+
+    // [CHECK] dataの型がunkown
+    const data = await response.json();
+    console.log(data.choices[0].message.content);
+
+}
+
+callOpenAI();
